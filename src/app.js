eaa7ad3e35b75c8ff93b92dbc9a22cb5a3ceb87c
Ramya Maddipati <ramyam@cybage.com>
2025-05-29 12:50:21 +0530

    secrets

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 console.log("Hello, World!");
 function handleLogin(username, password) {
-    return username === "admin" && password === "password123";
+    return username === "admin" && password === "admin";
 }
 // Attach handleLogin to the global window object
 window.handleLogin = handleLogin;
